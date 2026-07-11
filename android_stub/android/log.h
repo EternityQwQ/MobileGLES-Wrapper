7e9bffd0422d83cd37589a0288b91540cf8ba4e7
Marshlight0 <Marshlight0@users.noreply.github.com>
2026-07-11 02:46:40 +0000

    Revert "Merge pull request #44 from EternityQwQ/trae/agent-vEtvjd"
    This reverts commit c76595139c54f7bdd5cb6837567355ed37c8868b.

--- android_stub/android/log.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#define ANDROID_LOG_DEBUG 3
-#define ANDROID_LOG_WARN 5
-#define ANDROID_LOG_ERROR 6
-static inline int __android_log_print(int, const char*, const char*, ...) { return 0; }
